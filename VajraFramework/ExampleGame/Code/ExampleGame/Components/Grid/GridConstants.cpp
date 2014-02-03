c789aa969ebd84a78591a1e2d96d38cc25bab307
MatthewKaufmann <mckaufma1@gmail.com>
2014-02-03 15:01:03 -0500

    HALF_CELL is now actually half of a cell

--- VajraFramework/ExampleGame/Code/ExampleGame/Components/Grid/GridConstants.cpp
+++ VajraFramework/ExampleGame/Code/ExampleGame/Components/Grid/GridConstants.cpp
@@ -5,4 +5,4 @@
 
 #include "ExampleGame/Components/Grid/GridConstants.h"
 
-const glm::vec3 HALF_CELL = glm::vec3(CELL_SIZE, GROUND_Y, -CELL_SIZE);
+const glm::vec3 HALF_CELL = glm::vec3(CELL_SIZE / 2.0f, GROUND_Y, -CELL_SIZE / 2.0f);
