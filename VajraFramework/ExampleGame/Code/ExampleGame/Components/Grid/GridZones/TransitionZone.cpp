c193547ac1bb417943033a3dd2100e21baea963c
MatthewKaufmann <mckaufma1@gmail.com>
2014-02-06 16:15:57 -0500

    One transition zone now covers both sides of the transition.

--- VajraFramework/ExampleGame/Code/ExampleGame/Components/Grid/GridZones/TransitionZone.cpp
+++ VajraFramework/ExampleGame/Code/ExampleGame/Components/Grid/GridZones/TransitionZone.cpp
@@ -3,7 +3,6 @@
 //  Created by Matt Kaufmann on 01/31/14.
 //
 
-#include "ExampleGame/Components/Grid/GridCell.h"
 #include "ExampleGame/Components/Grid/GridManager.h"
 #include "ExampleGame/Components/Grid/GridNavigator.h"
 #include "ExampleGame/Components/Grid/GridZones/TransitionZone.h"
@@ -33,26 +32,52 @@ void TransitionZone::HandleMessage(MessageChunk messageChunk) {
 	}
 }
 
-void TransitionZone::SetDestination(int cellX, int cellZ) {
+void TransitionZone::SetFirstDestination(int cellX, int cellZ) {
 	GridCell* cell = SINGLETONS->GetGridManager()->GetGrid()->GetCell(cellX, cellZ);
-	if (cell != nullptr) {
-		this->destX = cell->x;
-		this->destZ = cell->z;
-	}
+	this->SetFirstDestination(cell);
 }
 
-void TransitionZone::SetDestination(glm::vec3 worldPos) {
-	GridCell* cell = SINGLETONS->GetGridManager()->GetGrid()->GetCell(worldPos);
-	if (cell != nullptr) {
-		this->destX = cell->x;
-		this->destZ = cell->z;
-	}
+void TransitionZone::SetFirstDestination(glm::vec3 worldPosition) {
+	GridCell* cell = SINGLETONS->GetGridManager()->GetGrid()->GetCell(worldPosition);
+	this->SetFirstDestination(cell);
+}
+
+void TransitionZone::SetFirstDestination(GridCell* cell) {
+	this->destCell1 = cell;
+}
+
+void TransitionZone::SetSecondDestination(int cellX, int cellZ) {
+	GridCell* cell = SINGLETONS->GetGridManager()->GetGrid()->GetCell(cellX, cellZ);
+	this->SetSecondDestination(cell);
+}
+
+void TransitionZone::SetSecondDestination(glm::vec3 worldPosition) {
+	GridCell* cell = SINGLETONS->GetGridManager()->GetGrid()->GetCell(worldPosition);
+	this->SetSecondDestination(cell);
+}
+
+void TransitionZone::SetSecondDestination(GridCell* cell) {
+	this->destCell2 = cell;
+}
+
+void TransitionZone::SetDestinations(int cellX1, int cellZ1, int cellX2, int cellZ2) {
+	this->SetFirstDestination(cellX1, cellZ1);
+	this->SetSecondDestination(cellX2, cellZ2);
+}
+
+void TransitionZone::SetDestinations(glm::vec3 worldPosition1, glm::vec3 worldPosition2) {
+	this->SetFirstDestination(worldPosition1);
+	this->SetSecondDestination(worldPosition2);
+}
+
+void TransitionZone::SetDestinations(GridCell* cell1, GridCell* cell2) {
+	this->SetFirstDestination(cell1);
+	this->SetSecondDestination(cell2);
 }
 
 void TransitionZone::init() {
-	this->zoneType = GRID_ZONE_TYPE_TRANSITION;
-	this->destX = -1;
-	this->destZ = -1;
+	this->destCell1 = nullptr;
+	this->destCell2 = nullptr;
 
 	this->addSubscriptionToMessageType(MESSAGE_TYPE_GRID_ZONE_ENTERED, this->GetTypeId(), false);
 }
@@ -68,7 +93,17 @@ void TransitionZone::onUnitEnteredZone(ObjectIdType id) {
 	GridNavigator* gNav = gObj->GetComponent<GridNavigator>();
 	ASSERT(gNav != nullptr, "Object with id %d has GridNavigator component", id);
 
-	if (IsCellWithinZone(gNav->GetDestination())) {
-		gNav->SetDestination(this->destX, this->destZ);
+	GridCell* unitCell = gNav->GetCurrentCell();
+	GridRoom* unitRoom = SINGLETONS->GetGridManager()->GetGrid()->GetRoom(unitCell);
+	GridRoom* destRoom1 = SINGLETONS->GetGridManager()->GetGrid()->GetRoom(destCell1);
+
+	if (unitRoom != destRoom1) {
+		// If the first destination and the unit are not in the same room, send the unit to that destination
+		gNav->SetDestination(this->destCell1);
+	}
+	else {
+		// If the first destination is in the same room, send the unit to the other one instead
+		// (Hopefully they won't be the same room)
+		gNav->SetDestination(this->destCell2);
 	}
 }
