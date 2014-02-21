d289421984208cb9bb1a78bc8ca3303238162ab5
Alex R. Hogue <alex.r.hogue@gmail.com>
2014-02-20 22:05:40 -0500

    Debug level loaded

--- VajraFramework/ExampleGame/Code/ExampleGame/Ui/TouchHandlers/MainMenuTouchHandlers.h
+++ VajraFramework/ExampleGame/Code/ExampleGame/Ui/TouchHandlers/MainMenuTouchHandlers.h
@@ -3,13 +3,17 @@
 
 #include "Vajra/Engine/Ui/UiTouchHandlers/UiTouchHandlers.h"
 
+class UiElement;
 class MainMenuTouchHandlers : public UiTouchHandlers {
 public:
+	MainMenuTouchHandlers();
 	// @Override
 	virtual void OnTouchDownHandlers(UiObject* uiObject, Touch touch);
 	virtual void OnTouchMoveHandlers(UiObject* uiObject, Touch touch);
 	virtual void OnTouchUpHandlers  (UiObject* uiObject, Touch touch);
 private:
+	void createMissionMenu();
+	UiElement* missionRoot;
 };
 
 #endif // MAINMENU_TOUCH_HANDLERS_H
