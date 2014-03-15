1da84b163411e4bcba1c9c7a6fa1722b800f1261
cosmiczilch <cosmiczilch@gmail.com>
2014-03-15 04:10:13 -0400

    Adding settings in options menu.

---
+++ VajraFramework/ExampleGame/Code/ExampleGame/Ui/TouchHandlers/SettingsMenuTouchHandlers.cpp
@@ -0,0 +1,13 @@
+#include "ExampleGame/Ui/TouchHandlers/SettingsMenuTouchHandlers.h"
+
+SettingsMenuTouchHandlers::SettingsMenuTouchHandlers() {
+}
+
+void SettingsMenuTouchHandlers::OnTouchDownHandlers(UiObject* uiObject, Touch touch) {
+}
+
+void SettingsMenuTouchHandlers::OnTouchMoveHandlers(UiObject* uiObject, Touch touch) {
+}
+
+void SettingsMenuTouchHandlers::OnTouchUpHandlers(UiObject* uiObject, Touch touch) {
+}
