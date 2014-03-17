c3c380dfe32335be75d4f12455f8ad16e44ade54
cosmiczilch <cosmiczilch@gmail.com>
2014-03-17 11:03:10 -0400

    Fix build.

--- VajraFramework/Vajra/Framework/Settings/DesktopSettingsHelper.cpp
+++ VajraFramework/Vajra/Framework/Settings/DesktopSettingsHelper.cpp
@@ -6,6 +6,11 @@
 
 #include <string>
 
+bool DoesSettingsFileExist() {
+	// TODO [Hack] Return true we are sure that the settings.xml file will always exist on desktop or the default file will have been copied over from GameResources
+	return true;
+}
+
 void GetSettingsFileHandleForReading(std::ifstream& file) {
 	std::string filePath = FRAMEWORK->GetFileSystemUtils()->GetDeviceSettingsResourcesPath() + SETTINGS_FILE_NAME;
 	file.open(filePath);
