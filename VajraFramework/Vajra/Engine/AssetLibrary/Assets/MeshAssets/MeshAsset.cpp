fb48b9c7889112df789c7621035062ddff791740
cosmiczilch <cosmiczilch@gmail.com>
2014-04-01 14:57:57 -0400

    Move a bunch of duplicated code between MeshAsset and RenderBatch_static to SimpleMesh.

--- VajraFramework/Vajra/Engine/AssetLibrary/Assets/MeshAssets/MeshAsset.cpp
+++ VajraFramework/Vajra/Engine/AssetLibrary/Assets/MeshAssets/MeshAsset.cpp
@@ -71,6 +71,7 @@ void MeshAsset::LoadAsset() {
 	if (out_textureFileName != "") {
 		this->material->SetTextureFilePath(out_textureFileName);
 	}
+	SimpleMesh::materialRef = this->material;
 
 	if (out_armatureFilePath != "") {
 		this->armatureFilePath = out_armatureFilePath;
@@ -172,31 +173,7 @@ void MeshAsset::InitBoneWeightInfluencesData(
 
 
 void MeshAsset::MakeVBOs() {
-    if (this->vertices != nullptr) {
-		GLCALL(glGenBuffers, 1, &this->vboPositions);
-		GLCALL(glBindBuffer, GL_ARRAY_BUFFER, this->vboPositions);
-		GLCALL(glBufferData, GL_ARRAY_BUFFER, sizeof(glm::vec3) * this->numVertices, this->vertices, GL_STATIC_DRAW);
-    } else {
-        FRAMEWORK->GetLogger()->errlog("ERROR: Uninited vertices");
-        return;
-    }
-
-    if (this->normals != nullptr) {
-		GLCALL(glGenBuffers, 1, &this->vboNormals);
-		GLCALL(glBindBuffer, GL_ARRAY_BUFFER, this->vboNormals);
-		GLCALL(glBufferData, GL_ARRAY_BUFFER, sizeof(glm::vec3) * this->numVertices, this->normals, GL_STATIC_DRAW);
-    } else {
-        FRAMEWORK->GetLogger()->errlog("ERROR: Uninited normals");
-        return;
-    }
-
-    if (this->textureCoords != nullptr) {
-		GLCALL(glGenBuffers, 1, &this->vboTextureCoords);
-		GLCALL(glBindBuffer, GL_ARRAY_BUFFER, this->vboTextureCoords);
-		GLCALL(glBufferData, GL_ARRAY_BUFFER, sizeof(glm::vec2) * this->numVertices, this->textureCoords, GL_STATIC_DRAW);
-    } else {
-        ASSERT(!this->material->HasTexture(), "Texture coords missing because model has no texture");
-    }
+	SimpleMesh::makeVBOs();
 
     if (this->boneIndices != nullptr) {
 		GLCALL(glGenBuffers, 1, &this->vboBoneIndices);
@@ -210,27 +187,12 @@ void MeshAsset::MakeVBOs() {
 		GLCALL(glBufferData, GL_ARRAY_BUFFER, sizeof(glm::vec4) * this->numVertices, this->boneWeights, GL_STATIC_DRAW);
     }
 
-    if (this->indices.size() != 0) {
-		GLCALL(glGenBuffers, 1, &this->vboIndices);
-		GLCALL(glBindBuffer, GL_ELEMENT_ARRAY_BUFFER, this->vboIndices);
-		GLCALL(glBufferData, GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * this->indices.size(), &this->indices[0], GL_STATIC_DRAW);
-    } else {
-        FRAMEWORK->GetLogger()->errlog("ERROR: Uninited indices");
-        return;
-    }
-
     FRAMEWORK->GetLogger()->errlog("\nVBOs made successfully");
 }
 
 void MeshAsset::Draw() {
-    if (this->vboPositions == 0 || this->vboNormals == 0 || this->vboIndices == 0) {
-        FRAMEWORK->GetLogger()->errlog("ERROR: VBOs not made");
-        return;
-    }
-    if (this->vboTextureCoords == 0 && this->material->HasTexture()) {
-        FRAMEWORK->GetLogger()->errlog("ERROR: Texture VBOs not made");
-        return;
-    }
+	SimpleMesh::prepareVerticesToDraw();
+
     if (this->boneIndices != nullptr && this->vboBoneIndices == 0) {
         FRAMEWORK->GetLogger()->errlog("ERROR: Bone indices VBOs not made");
         return;
@@ -240,46 +202,8 @@ void MeshAsset::Draw() {
         return;
     }
 
-    // GLCALL(glBindBuffer, GL_ARRAY_BUFFER, this->vboPositions);
-    // GLCALL(glEnableClientState, GL_VERTEX_ARRAY);
-    // GLCALL(glVertexPointer, 3, GL_FLOAT, 0, 0);
-
     ShaderSet* currentShaderSet = FRAMEWORK->GetOpenGLWrapper()->GetCurrentShaderSet();
 
-    GLint positionHandle = currentShaderSet->GetHandle(SHADER_VARIABLE_VARIABLENAME_vPosition);
-    GLCALL(glEnableVertexAttribArray, positionHandle);
-    GLCALL(glBindBuffer, GL_ARRAY_BUFFER, this->vboPositions);
-    GLCALL(glVertexAttribPointer, positionHandle, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    //
-    // TODO [Hack] Do this better, maybe:
-    // No normals in depth pass:
-    if (!currentShaderSet->IsDepthPass()) {
-    	GLint normalHandle = currentShaderSet->GetHandle(SHADER_VARIABLE_VARIABLENAME_vNormal);
-    	GLCALL(glEnableVertexAttribArray, normalHandle);
-    	GLCALL(glBindBuffer, GL_ARRAY_BUFFER, this->vboNormals);
-    	GLCALL(glVertexAttribPointer, normalHandle, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    }
-    //
-    // TODO [Hack] Do this better, maybe:
-    // No materials in depth pass:
-    if (!currentShaderSet->IsDepthPass()) {
-    	this->material->WriteMaterialToShader();
-    	if (this->material->HasTexture()) {
-    		this->material->GetTextureAsset()->Draw(0);
-
-			GLint textureCoordsHandle = currentShaderSet->GetHandle(SHADER_VARIABLE_VARIABLENAME_uvCoords_in);
-    		GLCALL(glEnableVertexAttribArray, textureCoordsHandle);
-    		GLCALL(glBindBuffer, GL_ARRAY_BUFFER, this->vboTextureCoords);
-    		GLCALL(glVertexAttribPointer, textureCoordsHandle, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-    	} else {
-    		if (currentShaderSet->HasHandle(SHADER_VARIABLE_VARIABLENAME_uvCoords_in)) {
-				GLint textureCoordsHandle = currentShaderSet->GetHandle(SHADER_VARIABLE_VARIABLENAME_uvCoords_in);
-				GLCALL(glDisableVertexAttribArray, textureCoordsHandle);
-    		}
-    	}
-    }
-    //
     if (this->vboBoneIndices != 0) {
 		GLint boneIndicesHandle = currentShaderSet->GetHandle(SHADER_VARIABLE_VARIABLENAME_boneIndices);
 		GLCALL(glEnableVertexAttribArray, boneIndicesHandle);
@@ -293,43 +217,22 @@ void MeshAsset::Draw() {
 		GLCALL(glBindBuffer, GL_ARRAY_BUFFER, this->vboBoneWeights);
 		GLCALL(glVertexAttribPointer, boneWeightsHandle, 4, GL_FLOAT, GL_FALSE, 0, 0);
     }
-    //
-    GLCALL(glBindBuffer, GL_ELEMENT_ARRAY_BUFFER, this->vboIndices);
-    GLCALL(glDrawElements, this->meshGlRenderingMode, this->indices.size(), GL_UNSIGNED_INT, (void*)0);
+
+	SimpleMesh::draw();
 
     return;
 }
 
 
 void MeshAsset::init() {
-    this->vertices = nullptr;
-    this->normals = nullptr;
-    this->textureCoords = nullptr;
-    this->material = nullptr;
-    //
     this->boneIndices = nullptr;
     this->boneWeights = nullptr;
 
-    this->meshGlRenderingMode = GL_TRIANGLES;
-
-    this->vboPositions = 0;
-    this->vboNormals = 0;
-    this->vboTextureCoords = 0;
-    this->vboIndices = 0;
     this->vboBoneIndices = 0;
     this->vboBoneWeights = 0;
 }
 
 void MeshAsset::destroy() {
-    if (this->vertices != nullptr) {
-        delete this->vertices;
-    }
-    if (this->normals != nullptr) {
-        delete this->normals;
-    }
-    if (this->textureCoords != nullptr) {
-        delete this->textureCoords;
-    }
     if (this->boneIndices != nullptr) {
         delete this->boneIndices;
     }
@@ -337,18 +240,6 @@ void MeshAsset::destroy() {
         delete this->boneWeights;
     }
 
-    if (this->vboPositions != 0) {
-		GLCALL(glDeleteBuffers, 1, &this->vboPositions);
-    }
-    if (this->vboNormals != 0) {
-		GLCALL(glDeleteBuffers, 1, &this->vboNormals);
-    }
-    if (this->vboTextureCoords != 0) {
-		GLCALL(glDeleteBuffers, 1, &this->vboTextureCoords);
-    }
-    if (this->vboIndices != 0) {
-		GLCALL(glDeleteBuffers, 1, &this->vboIndices);
-    }
     if (this->vboBoneIndices != 0) {
 		GLCALL(glDeleteBuffers, 1, &this->vboBoneIndices);
     }
---
+++ VajraFramework/Vajra/Engine/Components/DerivedComponents/Renderer/SimpleMesh.h
@@ -0,0 +1,47 @@
+#ifndef SIMPLE_MESH_H
+#define SIMPLE_MESH_H
+
+#include "Vajra/Engine/AssetLibrary/Assets/MeshAssets/Material/Material.h"
+#include "Vajra/Utilities/OpenGLIncludes.h"
+
+#include "Libraries/glm/glm.hpp"
+
+#include <string>
+#include <vector>
+
+class SimpleMesh {
+public:
+	SimpleMesh();
+	~SimpleMesh();
+
+protected:
+	GLuint vboPositions;
+	GLuint vboNormals;
+	GLuint vboTextureCoords;
+	//
+	GLuint vboIndices;
+
+	int numVertices;
+
+	glm::vec3* vertices;
+	glm::vec3* normals;
+	glm::vec2* textureCoords;
+	//
+	std::vector<unsigned int> indices;
+
+	Material* materialRef;
+
+	GLenum meshGlRenderingMode;
+
+	void makeVBOs();
+
+	void prepareVerticesToDraw();
+	void draw();
+
+private:
+	void init();
+	void destroy();
+
+};
+
+#endif // SIMPLE_MESH_H
