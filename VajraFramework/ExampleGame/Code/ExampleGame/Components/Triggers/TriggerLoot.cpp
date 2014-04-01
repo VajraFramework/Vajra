37715824785fc73b9df76d708783477c545bfe04
Alex R. Hogue <alex.r.hogue@gmail.com>
2014-04-01 14:56:27 -0400

    loot now bounces up and explodes

--- VajraFramework/ExampleGame/Code/ExampleGame/Components/Triggers/TriggerLoot.cpp
+++ VajraFramework/ExampleGame/Code/ExampleGame/Components/Triggers/TriggerLoot.cpp
@@ -1,10 +1,29 @@
 #include "ExampleGame/Components/Triggers/TriggerLoot.h"
+
+#include "Vajra/Common/Messages/CustomMessageDatas/MessageData1S1I1F.h"
 #include "Vajra/Engine/Components/DerivedComponents/Transform/Transform.h"
 #include "Vajra/Engine/Core/Engine.h"
+#include "Vajra/Engine/ParticleSystems/ParticleSystem.h"
+#include "Vajra/Engine/Prefabs/PrefabLoader.h"
 #include "Vajra/Engine/SceneGraph/SceneGraph3D.h"
 #include "Vajra/Engine/Tween/Tween.h"
+#include "Vajra/Framework/DeviceUtils/FileSystemUtils/FileSystemUtils.h"
 #include "Vajra/Utilities/MathUtilities.h"
 
+// Tween callbacks
+void lootNumberTweenCallback(float /*fromNumber*/, float toNumber, float currentNumber, std::string tweenClipName, MessageData1S1I1F* userParams) {
+	GameObject* go = ENGINE->GetSceneGraph3D()->GetGameObjectById(userParams->i);
+	if(go != nullptr) {
+		TriggerLoot* loot = go->GetComponent<TriggerLoot>();
+		if(loot != nullptr) {
+			float yOffset = cos(currentNumber);
+			go->GetTransform()->Translate(0.0f, loot->startPos.y + yOffset, 0.0f);
+			if(currentNumber == toNumber) {
+				loot->lootTweenEnd();
+			}
+		}
+	}
+}
 TriggerLoot::TriggerLoot() : Triggerable() {
 	this->init();
 }
@@ -18,7 +37,9 @@ TriggerLoot::~TriggerLoot() {
 }
 
 void TriggerLoot::init() {
-	Triggerable::SetTriggerType("Once");
+	// create the partile effect
+	GameObject* particleEffect = PrefabLoader::InstantiateGameObjectFromPrefab(FRAMEWORK->GetFileSystemUtils()->GetDevicePrefabsResourcesPath() + "dustexplosion.prefab", ENGINE->GetSceneGraph3D());
+	this->particleEffectId = particleEffect->GetId();
 }
 
 void TriggerLoot::destroy() {
@@ -33,6 +54,32 @@ void TriggerLoot::SubscribeToParentSwitch() {
 	Triggerable::SubscribeToParentSwitch();
 }
 
-void TriggerLoot::onSwitchToggled(bool switchState) {
-	//this->startTransformation(switchState);
+void TriggerLoot::SetMoneyValue(int v) {
+	this->moneyValue = v;
 }
+
+void TriggerLoot::onSwitchToggled(bool switchState) {
+	this->startPos = this->GetObject()->GetComponent<Transform>()->GetPosition();
+	// Perform a position tween
+	MessageData1S1I1F* params = new MessageData1S1I1F();
+	params->i = this->GetObject()->GetId();
+	ENGINE->GetTween()->TweenToNumber(0.0f, 1.8f, .3f, INTERPOLATION_TYPE_LINEAR, true, false, true, "lootBounce", NUMBER_TWEEN_AFFILIATION_SCENEGRAPH_3D, params, lootNumberTweenCallback);
+}
+
+void TriggerLoot::lootTweenEnd() {
+	// TODO add in number tween and particle burst
+	GameObject* particleEffectObj = ENGINE->GetSceneGraph3D()->GetGameObjectById(this->particleEffectId);
+	if (particleEffectObj != nullptr) {
+		// Move the effect to this object's position
+		Transform* myTrans = this->GetObject()->GetComponent<Transform>();
+		Transform* effectTrans = particleEffectObj->GetTransform();
+		effectTrans->SetPositionWorld(myTrans->GetPositionWorld());
+
+		ParticleSystem* particleSystem = particleEffectObj->GetComponent<ParticleSystem>();
+		VERIFY(particleSystem != nullptr, "particle effect prefab has a particle system on it");
+		particleSystem->Play();
+	}
+	// Hide the mesh
+	((GameObject*)this->GetObject())->SetVisible(false);
+}
+
--- VajraFramework/ExampleGame/Code/ExampleGame/Components/Triggers/TriggerLoot.h
+++ VajraFramework/ExampleGame/Code/ExampleGame/Components/Triggers/TriggerLoot.h
@@ -3,6 +3,8 @@
 
 #include "ExampleGame/Components/Triggers/Triggerable.h"
 
+class MessageData1S1I1F;
+
 //[[COMPONENT]]//
 class TriggerLoot : public Triggerable {
 public:
@@ -17,12 +19,24 @@ public:
 	//[[PROPERTY]]//
 	virtual void SubscribeToParentSwitch();  // Subscribe to the switch on the parent object, can be used in XML prefabs
 
+	//[[PROPERTY]]//
+	void SetMoneyValue(int v);
+
 protected:
 	virtual void onSwitchToggled(bool switchState);
 
 private:
 	void init();
 	void destroy();
+
+	ObjectIdType particleEffectId;
+	
+	glm::vec3 startPos;
+	int moneyValue;
+
+	void lootTweenEnd();
+
+	friend void lootNumberTweenCallback(float fromNumber, float toNumber, float currentNumber, std::string tweenClipName, MessageData1S1I1F* userParams);
 };
 
 #endif // TriggerLoot_H
