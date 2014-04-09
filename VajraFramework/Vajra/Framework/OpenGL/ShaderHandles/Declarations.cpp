68fd790f1f167c8377d9e6e950d7a71bef3053f2
cosmiczilch <cosmiczilch@gmail.com>
2014-04-09 14:37:21 -0400

    Got rid of unnecessary variables in shaders for normal maps. Fixed bone texture vertex shader.

--- VajraFramework/Vajra/Framework/OpenGL/ShaderHandles/Declarations.cpp
+++ VajraFramework/Vajra/Framework/OpenGL/ShaderHandles/Declarations.cpp
@@ -141,8 +141,8 @@ Shader_variable_variablename_id_t GetShaderVariableVariableNameIdFromString(std:
 	if (s == "AdditionalLight0Direction") {
 		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight0Direction;
 	}
-	if (s == "AdditionalLight0Direction_tangentspace") {
-		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight0Direction_tangentspace;
+	if (s == "AdditionalLight0Direction_out") {
+		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight0Direction_out;
 	}
 	if (s == "AdditionalLight0Ambient") {
 		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight0Ambient;
@@ -153,8 +153,8 @@ Shader_variable_variablename_id_t GetShaderVariableVariableNameIdFromString(std:
 	if (s == "AdditionalLight1Direction") {
 		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight1Direction;
 	}
-	if (s == "AdditionalLight1Direction_tangentspace") {
-		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight1Direction_tangentspace;
+	if (s == "AdditionalLight1Direction_out") {
+		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight1Direction_out;
 	}
 	if (s == "AdditionalLight1Ambient") {
 		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight1Ambient;
@@ -165,8 +165,8 @@ Shader_variable_variablename_id_t GetShaderVariableVariableNameIdFromString(std:
 	if (s == "AdditionalLight2Direction") {
 		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight2Direction;
 	}
-	if (s == "AdditionalLight2Direction_tangentspace") {
-		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight2Direction_tangentspace;
+	if (s == "AdditionalLight2Direction_out") {
+		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight2Direction_out;
 	}
 	if (s == "AdditionalLight2Ambient") {
 		return SHADER_VARIABLE_VARIABLENAME_AdditionalLight2Ambient;
@@ -218,21 +218,6 @@ Shader_variable_variablename_id_t GetShaderVariableVariableNameIdFromString(std:
 	}
 	if (s == "normalMap") {
 		return SHADER_VARIABLE_VARIABLENAME_normalMap;
-	}
-	if (s == "LightDirection_cameraspace") {
-		return SHADER_VARIABLE_VARIABLENAME_LightDirection_cameraspace;
-	}
-	if (s == "EyeDirection_cameraspace") {
-		return SHADER_VARIABLE_VARIABLENAME_EyeDirection_cameraspace;
-	}
-	if (s == "LightDirection_tangentspace") {
-		return SHADER_VARIABLE_VARIABLENAME_LightDirection_tangentspace;
-	}
-	if (s == "vTangent_out") {
-		return SHADER_VARIABLE_VARIABLENAME_vTangent_out;
-	}
-	if (s == "vBitangent_out") {
-		return SHADER_VARIABLE_VARIABLENAME_vBitangent_out;
 	}
 	if (s == "TBNmatrix") {
 		return SHADER_VARIABLE_VARIABLENAME_TBNmatrix;
@@ -305,15 +290,15 @@ std::string GetStringForShaderVariableVariableNameId(Shader_variable_variablenam
 	case SHADER_VARIABLE_VARIABLENAME_MaterialDiffuseColor_out: return "MaterialDiffuseColor_out";
 	case SHADER_VARIABLE_VARIABLENAME_MaterialSpecularColor_out: return "MaterialSpecularColor_out";
 	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight0Direction: return "AdditionalLight0Direction";
-	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight0Direction_tangentspace: return "AdditionalLight0Direction_tangentspace";
+	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight0Direction_out: return "AdditionalLight0Direction_out";
 	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight0Ambient: return "AdditionalLight0Ambient";
 	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight0Diffuse: return "AdditionalLight0Diffuse";
 	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight1Direction: return "AdditionalLight1Direction";
-	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight1Direction_tangentspace: return "AdditionalLight1Direction_tangentspace";
+	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight1Direction_out: return "AdditionalLight1Direction_out";
 	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight1Ambient: return "AdditionalLight1Ambient";
 	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight1Diffuse: return "AdditionalLight1Diffuse";
 	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight2Direction: return "AdditionalLight2Direction";
-	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight2Direction_tangentspace: return "AdditionalLight2Direction_tangentspace";
+	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight2Direction_out: return "AdditionalLight2Direction_out";
 	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight2Ambient: return "AdditionalLight2Ambient";
 	case SHADER_VARIABLE_VARIABLENAME_AdditionalLight2Diffuse: return "AdditionalLight2Diffuse";
 	case SHADER_VARIABLE_VARIABLENAME_pointSize: return "pointSize";
@@ -331,11 +316,6 @@ std::string GetStringForShaderVariableVariableNameId(Shader_variable_variablenam
 	case SHADER_VARIABLE_VARIABLENAME_vTangent: return "vTangent";
 	case SHADER_VARIABLE_VARIABLENAME_vBitangent: return "vBitangent";
 	case SHADER_VARIABLE_VARIABLENAME_normalMap: return "normalMap";
-	case SHADER_VARIABLE_VARIABLENAME_LightDirection_cameraspace: return "LightDirection_cameraspace";
-	case SHADER_VARIABLE_VARIABLENAME_EyeDirection_cameraspace: return "EyeDirection_cameraspace";
-	case SHADER_VARIABLE_VARIABLENAME_LightDirection_tangentspace: return "LightDirection_tangentspace";
-	case SHADER_VARIABLE_VARIABLENAME_vTangent_out: return "vTangent_out";
-	case SHADER_VARIABLE_VARIABLENAME_vBitangent_out: return "vBitangent_out";
 	case SHADER_VARIABLE_VARIABLENAME_TBNmatrix: return "TBNmatrix";
 	case SHADER_VARIABLE_VARIABLENAME_invalid: return "invalid";
 	// default: DO NOT ADD A DEFAULT HERE. It'll help catch the case where the enum is extended but not handled here
