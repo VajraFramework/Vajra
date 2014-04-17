1237f9567cd42d82ed2adafaf3af87bab38b2ba9
Alex R. Hogue <alex.r.hogue@gmail.com>
2014-04-17 17:04:54 -0400

    new ui

--- VajraFramework/ExampleGame/Code/ExampleGame/Ui/MenuManager/MenuManager.cpp
+++ VajraFramework/ExampleGame/Code/ExampleGame/Ui/MenuManager/MenuManager.cpp
@@ -38,8 +38,12 @@ void menuManagerNumberTweenCallback(float /* fromNumber */, float toNumber , flo
 	} else if (tweenClipName == "delayPreMenuOpen") {
 		UiObject* preMenuScreen = (UiObject*)ObjectRegistry::GetObjectByName("preMenu");
 		VERIFY(preMenuScreen != nullptr, "preMenuScreen to show is not null");
+		preMenuScreen->SetVisible(false);
+
+		preMenuScreen = (UiObject*)ObjectRegistry::GetObjectByName("postWinGame");
+		VERIFY(preMenuScreen != nullptr, "preMenuScreen to show is not null");
 		preMenuScreen->SetVisible(true);
-		SINGLETONS->GetMenuManager()->TweenInUiObject(preMenuScreen);
+		//SINGLETONS->GetMenuManager()->TweenInUiObject(preMenuScreen);
 	}
 
 }
